6fd2156442a0ab4abca6441c07d521e362ced2f1
Quoc Huy Nguyen <quochuynguyen@Quocs-MacBook-Pro.local>
2023-11-07 10:31:32 +0200

    update to AddScheduleDialog.tsx

--- src/components/TruckComponents/AddScheduleDialog.tsx
+++ src/components/TruckComponents/AddScheduleDialog.tsx
@@ -1,12 +1,25 @@
-import { Button, Dialog, DialogActions, DialogContent, DialogTitle, FormControl, InputLabel, MenuItem, Select, SelectChangeEvent } from "@mui/material";
+import { 
+   Button, 
+   Dialog, 
+   DialogActions, 
+   DialogContent, 
+   DialogTitle, 
+   FormControl, 
+   InputLabel, 
+   MenuItem, 
+   Select 
+} from "@mui/material";
 import { FC, useState } from "react";
+import dayjs from "dayjs";
+import { SnackbarProvider, useSnackbar } from "notistack";
+
 import { ConnectionType, DataRow, TruckSchedule } from "types";
 
 import _data from "../../data/data.json";
 import _mapData from "../../data/mapData.json";
-import dayjs from "dayjs";
 
 interface CalDistance {
+   path: string[];
    distance: number;
 }
 
@@ -14,7 +27,9 @@ function calculateDistance(
    startPosition: string,
    mapData: ConnectionType[],
    endPosition: string): CalDistance {
-
+   
+   // console.log("end: ", endPosition);
+   
    // Create distance map
    const distanceMap: Map<string, number> = new Map();
    mapData.forEach((item, index) => {
@@ -22,24 +37,74 @@ function calculateDistance(
    });
    distanceMap.set(mapData[0].name, 0);
 
-   console.log(distanceMap);
-
-   // Create visited map
-   const visited: Map<string, boolean> = new Map();
+   // Create previous map
+   const prev: Map<string, string | undefined> = new Map();
    mapData.forEach((item, index) => {
-      visited.set(item.name, false);
+      prev.set(item.name, undefined);
    });
 
-   console.log(visited);
+   // queue of vertexes
+   // TODO: can be updated to priority queue
+   let queue: {name: string, distance: number}[] =  [];
+  
+   queue.push({name: startPosition, distance: 0});
 
-   console.log(endPosition);
+   const path: string[] = []; 
 
-   const queue: string[] = [];
-   
-   // begin dijsktra
-   queue.push()
+   while(queue.length > 0) {
+      // Find the smallest in queue
+      let minDis = Infinity;
+      let minKey = "";
+      queue.forEach((item, index) => {
+         if(item.distance < minDis) {
+            minKey = item.name;
+            minDis = item.distance;
+         }
+      });
 
-   return( { distance: 0 } );
+      // console.log(minKey);
+      // console.log(queue.toString());
+      // console.log("endPosition: ", endPosition);
+
+      if(minKey === endPosition) {
+         // WE ARE DONE
+         let curKey = minKey;
+         while(prev.get(curKey) !== undefined) {
+            const temp = prev.get(curKey);
+            if(temp === undefined) {
+               break;
+            } else {
+               path.push(curKey);
+               curKey = temp;
+            }
+         }
+         break;
+      }
+      queue = queue.filter((item) => item.name !== minKey);
+      if(minKey !== "" || distanceMap.get(minKey) !== Infinity) {
+         const minKeyMap = mapData.find((item) => item.name === minKey);
+         if(minKeyMap === undefined) {
+            console.error("Error");
+         } else {
+            minKeyMap.connections.forEach((item, index) => {
+               let nextNode = item.name;
+               let candidate = distanceMap.get(minKey) as number + item.distance;
+               if(distanceMap.get(nextNode) as number > candidate) {
+                  distanceMap.set(nextNode, candidate);
+                  prev.set(nextNode, minKey);
+                  queue.push({name: nextNode, distance: candidate});
+               }
+            });
+         }
+      }
+   }
+
+   return( 
+      { 
+         path: path.concat(startPosition).reverse(),
+         distance: distanceMap.get(endPosition) as number 
+      } 
+   );
 }
 
 interface Props {
@@ -55,24 +120,40 @@ export const AddScheduleDialog: FC<Props> = (
 ) => {
 
    const data: DataRow[] = _data as DataRow[];
-   const [selectItem, setSelectItem] = useState(data[0]);
    const mapData: ConnectionType[] = _mapData as ConnectionType[];
+   const [curStartPosition, setCurStartPositon] = useState(truckStartPositon);
+
+
+   const { enqueueSnackbar } = useSnackbar();
+   const [selectItem, setSelectItem] = useState(data[0]);
+   const [numberOfContainer, setNumberOfContainer] = useState(0);
 
    const saveButtonOnClick = () => {
-      const endPosition = selectItem.Täytttöpiste.substring(0, 6);
-      const distance = calculateDistance(truckStartPositon, mapData, endPosition);
-      const newTruckSchedule: TruckSchedule = {
-         stopID: selectItem.Täytttöpiste,
-         packageID: selectItem.Kontti,
-         arriveTime: dayjs(),
-         distance: 0,
-         numberOfContainer: 0
+      if(numberOfContainer === 3) {
+         enqueueSnackbar("container is full");
+         handleClose();
+      } else {
+         const endPosition = selectItem.Täytttöpiste.substring(0, 6);
+         const result = calculateDistance(curStartPosition, mapData, endPosition);
+         console.log(result.distance);
+         console.log(result.path);
+         const newTruckSchedule: TruckSchedule = {
+            stopID: selectItem.Täytttöpiste,
+            packageID: selectItem.Kontti,
+            arriveTime: dayjs(),
+            distance: result.distance,
+            numberOfContainer: numberOfContainer + 1
+         }
+         console.log(result.path);
+         setCurStartPositon(endPosition);
+         setNumberOfContainer(numberOfContainer + 1);
+         setRows([...rows, newTruckSchedule]);
+         handleClose();
       }
-      setRows([...rows, newTruckSchedule]);
-      handleClose();
    }
 
    return (
+      <SnackbarProvider>
       <Dialog open={open} onClose={handleClose}>
          <DialogTitle> Add new truck schedule </DialogTitle>
          <DialogContent>
@@ -104,5 +185,6 @@ export const AddScheduleDialog: FC<Props> = (
             <Button onClick={saveButtonOnClick}>Save</Button>
          </DialogActions>
       </Dialog>
+      </SnackbarProvider>
    );
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { RouterProvider } from 'react-router-dom';
 import { RecoilRoot } from "recoil";
+import { SnackBarProvider } from "notistack";
+
+import './index.css';
 import router from 'routes/router';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
