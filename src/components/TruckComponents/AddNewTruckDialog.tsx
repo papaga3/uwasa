edfa6a5f996c1e0640e78c2e19b038cb606de2a7
Quoc Huy Nguyen <quochuynguyen@Quocs-MacBook-Pro.local>
2023-12-12 13:22:14 +0200

    small fix

--- src/components/TruckComponents/AddNewTruckDialog.tsx
+++ src/components/TruckComponents/AddNewTruckDialog.tsx
@@ -4,14 +4,22 @@ import {
    DialogActions, 
    DialogContent, 
    DialogTitle, 
+   FormControl, 
    FormLabel, 
+   InputLabel, 
+   MenuItem, 
+   Select, 
+   SelectChangeEvent, 
    TextField 
 } from "@mui/material";
 import { DateTimePicker } from "@mui/x-date-pickers";
 import { FC, useState } from "react";
 
 import _stationData from "../../data/station.json";
-import { Station } from "types";
+import { Station, Truck } from "types";
+import dayjs, { Dayjs } from "dayjs";
+import { useRecoilState, useSetRecoilState } from "recoil";
+import { truckListAtom } from "atom";
 
 interface Props {
    open: boolean;
@@ -23,9 +31,38 @@ export const AddNewTruckDialog: FC<Props> = (
 ) => {
    const stationData: Station[] = _stationData as Station[];
 
+   const setTruckList = useSetRecoilState(truckListAtom);
+
    const [truckName, setTruckName] = useState("");
    const [startPoint, setStartPoint] = useState("");
+   const [startTime, setStartTime] = useState<Dayjs | null>(null);
    
+   const handleChange = (event: SelectChangeEvent) => {
+      setStartPoint(event.target.value);
+   };
+
+   const saveButtonOnClick = () => {
+      let inputTime = startTime;
+      if(inputTime === null) {
+         inputTime = dayjs();
+      }
+
+      const truck: Truck = {
+         ID: truckName,
+         startPostion: startPoint,
+         startTime: inputTime,
+         schedule: [],
+      };
+
+      setTruckList((oldTruckList) => {
+         return (
+            [
+               ...oldTruckList, truck
+            ]
+         );
+      })
+   }
+
    return (
       <Dialog open={open} onClose={handleClose}>
          <DialogTitle> Add new truck </DialogTitle>
@@ -37,21 +74,49 @@ export const AddNewTruckDialog: FC<Props> = (
                      required={true}
                      sx={{mb: 3}}
                      onChange={(event) => setTruckName(event.target.value)}
+                     value={truckName}
                   />
                </div>
                <div>
-                  <FormLabel>Start Point</FormLabel>
-                  
+                  <FormControl sx={{ m: 1, minWidth: 120 }}>
+                     <InputLabel id="select-start-point-label">Start Point</InputLabel>
+                     <Select
+                        labelId="select-start-point-label"
+                        id="select-start-point"
+                        value={startPoint}
+                        label="Age"
+                        onChange={handleChange}
+                     > 
+                        { 
+                           stationData.map((item, index) => {
+                              // console.log("tt");
+                              return (
+                                 <MenuItem 
+                                    key={`select-start-point-${index}`} 
+                                    value={item.name}
+                                 >
+                                    {item.name}
+                                 </MenuItem>
+                              );
+                           })
+                        }
+                     </Select>
+                  </FormControl>
                </div>
                <div>
                   <FormLabel>Start time</FormLabel>
-                  <DateTimePicker />
+                  <DateTimePicker
+                     value={startTime}
+                     onChange={setStartTime}
+                     referenceDate={dayjs('2023-04-17T15:30')}
+                     format="DD.MM.YYYY HH:mm:ss"
+                  />
                </div>
                
             </form>
          </DialogContent>
          <DialogActions>
-            <Button onClick={() => {}}> Save </Button>
+            <Button onClick={saveButtonOnClick}> Save </Button>
          </DialogActions>
       </Dialog>
    );
